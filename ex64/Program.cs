d55f091ad3c8559a5fd5607b674d7e75192a125c
BuMax <max@butenko.in>
2022-12-25 23:55:00 +1000

    Exercise #64 completed

---
+++ ex64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Write("Введите число N: ");
+var n = Convert.ToInt32(Console.ReadLine());
+
+void GetNums(int number)
+{
+    if (number == 1)
+    {
+        Console.Write(1);
+        return;
+    }
+    Console.Write($"{number}, ");
+    GetNums(number - 1);
+}
+
+Console.Write($"N = {n} -> ");
+GetNums(n);
